d884ad5b1a1cc248e7ee09e46767f4ce175e5997
Lumine <463305152@qq.com>
2025-09-03 22:36:00 +0800

    update sign

--- src-tauri/src/content/sign/mod.rs
+++ src-tauri/src/content/sign/mod.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use crate::content::sign::{around_sign::AroundSign, badge_sign::BadgeSign, cache_sign::CacheSign, color_sign::ColorSign, ground_sign::GroundSign, line_sign::LineSign, path_sign::PathSign, sign::Sign, text_sign::TextSign, title_sign::TitleSign};
+use crate::content::sign::{
+    around_sign::AroundSign, badge_sign::BadgeSign, cache_sign::CacheSign, color_sign::ColorSign,
+    ground_sign::GroundSign, line_sign::LineSign, path_sign::PathSign, sign::Sign,
+    text_sign::TextSign, title_sign::TitleSign,
+};
 
-
-
-pub mod sign;
 pub mod around_sign;
 pub mod badge_sign;
 pub mod cache_sign;
@@ -13,6 +14,7 @@ pub mod figure_sign;
 pub mod ground_sign;
 pub mod line_sign;
 pub mod path_sign;
+pub mod sign;
 pub mod text_sign;
 pub mod title_sign;
 
@@ -29,65 +31,65 @@ pub enum SignType {
     TitleSign(TitleSign),
 }
 
-
-pub fn sign_derialize(msg:String) -> Vec<SignType> {
-     let mut res = Vec::new();
-    if msg == "-1"{
+pub fn sign_derialize(msg: String) -> Vec<SignType> {
+    let mut res = Vec::new();
+    if msg == "-1" {
         return res;
     }
 
-
-    let signs: Vec<&str> = msg.split(";").collect();
-   
-    let sign_type = signs[0];
-    match sign_type {
-        "AroundSign" => {
-            if let Ok(around_sign) = AroundSign::deserialize_str(signs) {
-                res.push(SignType::AroundSign(around_sign));
-            };
-        }
-        "BadgeSign" => {
-            if let Ok(badge_sign) = BadgeSign::deserialize_str(signs) {
-                res.push(SignType::BadgeSign(badge_sign));
-            };
-        }
-        "CacheSign" => {
-            if let Ok(cache_sign) = CacheSign::deserialize_str(signs) {
-                res.push(SignType::CacheSign(cache_sign));
-            };
-        }
-        "ColorSign" => {
-            if let Ok(color_sign) = ColorSign::deserialize_str(signs) {
-                res.push(SignType::ColorSign(color_sign));
-            };
-        }
-        "GroundSign" => {
-            if let Ok(ground_sign) = GroundSign::deserialize_str(signs) {
-                res.push(SignType::GroundSign(ground_sign));
-            };
-        }
-        "LineSign" => {
-            if let Ok(line_sign) = LineSign::deserialize_str(signs) {
-                res.push(SignType::LineSign(line_sign));
-            };
-        }
-        "PathSign" => {
-            if let Ok(path_sign) = PathSign::deserialize_str(signs) {
-                res.push(SignType::PathSign(path_sign));
-            };
-        }
-        "TextSign" => {
-            if let Ok(text_sign) = TextSign::deserialize_str(signs) {
-                res.push(SignType::TextSign(text_sign));
-            };
-        }
-        "TitleSign" => {
-            if let Ok(title_sign) = TitleSign::deserialize_str(signs) {
-                res.push(SignType::TitleSign(title_sign));
-            };
-        }
-        _ => {
+    let signs: Vec<&str> = msg.split(';').collect();
+    for sign in signs {
+        let sign_s: Vec<&str> = sign.split(',').collect();
+        let sign_type = sign_s[0];
+        match sign_type {
+            "AroundSign" => {
+                if let Ok(around_sign) = AroundSign::deserialize_str(sign_s) {
+                    res.push(SignType::AroundSign(around_sign));
+                };
+            }
+            "BadgeSign" => {
+                if let Ok(badge_sign) = BadgeSign::deserialize_str(sign_s) {
+                    res.push(SignType::BadgeSign(badge_sign));
+                };
+            }
+            "CacheSign" => {
+                if let Ok(cache_sign) = CacheSign::deserialize_str(sign_s) {
+                    res.push(SignType::CacheSign(cache_sign));
+                };
+            }
+            "ColorSign" => {
+                if let Ok(color_sign) = ColorSign::deserialize_str(sign_s) {
+                    res.push(SignType::ColorSign(color_sign));
+                };
+            }
+            "GroundSign" => {
+                if let Ok(ground_sign) = GroundSign::deserialize_str(sign_s) {
+                    res.push(SignType::GroundSign(ground_sign));
+                };
+            }
+            "LineSign" => {
+                if let Ok(line_sign) = LineSign::deserialize_str(sign_s) {
+                    res.push(SignType::LineSign(line_sign));
+                };
+            }
+            "PathSign" => {
+                if let Ok(path_sign) = PathSign::deserialize_str(sign_s) {
+                    res.push(SignType::PathSign(path_sign));
+                };
+            }
+            "TextSign" => {
+                if let Ok(text_sign) = TextSign::deserialize_str(sign_s) {
+                    res.push(SignType::TextSign(text_sign));
+                };
+            }
+            "TitleSign" => {
+                if let Ok(title_sign) = TitleSign::deserialize_str(sign_s) {
+                    res.push(SignType::TitleSign(title_sign));
+                };
+            }
+            _ => {}
         }
     }
+
     res
 }
